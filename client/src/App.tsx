d51d24e5c5875b7dd9f070e574065d0053139079
vanshverma721 <vvv.721781@gmail.com>
2024-07-26 16:40:43 +0530

    final commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,35 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import { Dashboard } from './pages/dashboard';
-import { Auth } from './pages/auth';
-import { FinancialRecordsProvider } from './contexts/financial-record-context';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Dashboard } from "./pages/dashboard";
+import { Auth } from "./pages/auth";
+import { FinancialRecordsProvider } from "./contexts/financial-record-context";
+import { SignedIn, UserButton } from "@clerk/clerk-react";
+// import { dark } from "@clerk/themes";
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-        <div className="app-container">
-          <Routes>
-            <Route path="/" element={<FinancialRecordsProvider><Dashboard /></FinancialRecordsProvider>} />
-            <Route path="/auth" element={<Auth />} />
-          </Routes>
+    <Router>
+      <div className="app-container">
+        <div className="navbar">
+          <Link to="/"> Dashboard</Link>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>
-      </BrowserRouter>
-    </>
-  )
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <FinancialRecordsProvider>
+                <Dashboard />
+              </FinancialRecordsProvider>
+            }
+          />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,20 +1,22 @@
-import {SignInButton, SignUpButton, UserButton, SignedIn, SignedOut} from '@clerk/clerk-react';
+import {
+    SignedIn,
+    SignedOut,
+    SignInButton,
+    SignUpButton,
+} from "@clerk/clerk-react";
+import { Navigate } from "react-router-dom";
 
 export const Auth = () => {
     return (
-
-        <div className='sign-in-container'>
-
+        <div className="sign-in-container">
             <SignedOut>
-                <SignUpButton mode='modal' />
-                <SignInButton mode='modal' />
+                <h1> Welcome to Your Own Personal Finance Tracker!</h1>
+                <SignUpButton mode="modal" />
+                <SignInButton mode="modal" />
             </SignedOut>
-
             <SignedIn>
-                <UserButton />
+                <Navigate to="/" />
             </SignedIn>
-
         </div>
-
     );
-}
+};
